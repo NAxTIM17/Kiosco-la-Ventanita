bde9c8dbe1bfa9895c878bbe46afd5406f857741
NAxTIM17 <naxtim14@gmail.com>
2023-11-10 04:17:29 -0300

    feat:Set Tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import BusinessRouter from './Components/BussinesRouter';
+import SideBar from './Components/Sidebar';
+
 
 const App = () => {
   return(
     <>
       <h1>midiu</h1>
+      <SideBar/>
       <BusinessRouter/>
     </>
   )
