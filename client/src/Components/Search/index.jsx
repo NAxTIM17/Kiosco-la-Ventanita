a605b30b899f0742cb10f2a0bf5029e1a0c3785f
NAxTIM17 <naxtim14@gmail.com>
2023-12-08 00:20:34 -0300

    feat: Change styles and layout inventory

--- client/src/Components/Search/index.jsx
+++ client/src/Components/Search/index.jsx
@@ -38,31 +38,17 @@ export default function SearchInput({filterText, onFilterTextChange}) {
       <>
     <div className="w-[340px] h-[240px] px-8 rounded-2xl flex justify-center items-center bg-gradient-to-tr from-pink-500 to-yellow-500 text-white container-Search">
       <Input
-        label="Search"
+        label="Buscar"
         isClearable
         radius="lg"
         classNames={{
           label: "text-black/50 dark:text-white/90",
           input: [
-            "bg-transparent",
             "text-black/90 dark:text-white/90",
             "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-          ],
-          innerWrapper: "bg-transparent",
-          inputWrapper: [
-            "shadow-xl",
-            "bg-default-200/50",
-            "dark:bg-default/60",
-            "backdrop-blur-xl",
-            "backdrop-saturate-200",
-            "hover:bg-default-200/70",
-            "dark:hover:bg-default/70",
-            "group-data-[focused=true]:bg-default-200/50",
-            "dark:group-data-[focused=true]:bg-default/60",
-            "!cursor-text",
-          ],
+          ]
         }}
-        placeholder="Type to search..."
+        placeholder="Que buscamos?"
         startContent={
           <SearchIcon className="text-black/50 mb-0.5 dark:text-black/90 text-slate-400 pointer-events-none flex-shrink-0" />
         }
--- client/src/pages/Inventory/index.jsx
+++ client/src/pages/Inventory/index.jsx
@@ -1,11 +1,23 @@
 import './inventory.css'
 import React from 'react';
-
+import SearchInput from '../../Components/Search';
 
 function Inventory(){
   return(
     <>
-    <h1>INVETARIO</h1>
+    <div className="inventory-container">
+      <div className="inventory-title">
+        <h1>INVETARIO</h1>
+      </div>
+      <div className="inventory-container-table">
+        <div className="container-table-search">
+            <SearchInput/>
+        </div>
+        <div className="container-table">
+
+        </div>
+      </div>
+    </div>
     </>
   )
    
