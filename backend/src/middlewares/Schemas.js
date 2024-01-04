6731a0ab405a15e8756a635466ff71748fbe81b5
NAxTIM17 <naxtim14@gmail.com>
2024-01-03 22:38:49 -0300

    feat: create schemas for products

--- backend/src/middlewares/Schemas.js
+++ backend/src/middlewares/Schemas.js
@@ -1,5 +1,9 @@
+import joi from 'joi'
 
+export const schemasProduct = joi.object({
+    nombre: joi.string().min(5).max(255).required(),
+    descripcion: joi.string().min(5).max(255).required(),
+    cantidad: joi.number().integer().positive().min(100).max(9999).required(),
+    precio: joi.number().precision().positive().min.apply(100).max(9999).required()
 
-export const schemasProduct = () =>{
-    
-}
+})
