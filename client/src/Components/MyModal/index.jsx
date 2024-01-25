f662162854f8efc720971ca790a147929ffae386
NAxTIM17 <naxtim14@gmail.com>
2024-01-25 20:43:44 -0300

    feat: completed the design of the shopping modal and fixed the button margin.

--- client/src/Components/MyModal/index.jsx
+++ client/src/Components/MyModal/index.jsx
@@ -14,11 +14,36 @@ export function MyModal({
     <>
       <motion.div className="modal-container">
         <motion.div layoutId={selectedId} className="modal">
-          <motion.div className="modal-content">{children}</motion.div>
-          <motion.div className="modal-buttons">
-            <div className="buttons">
-              <MyButton name={btnCloseText} onClick={() => setSelectedId(null)}/>
-              <MyButton name={btnAccionText} />
+          <motion.div className="modal-content">
+            <div className="modal-photo">
+              <img
+                className="photo"
+                src="https://purina.com.pe/sites/default/files/styles/webp/public/2022-10/Que_debes_saber_antes_de_adoptar_un_gatito.jpg.webp?itok=N2sS0lfp"
+                alt=""
+              />
+              <div className="increment">
+
+              </div>
+            </div>
+            <div className="modal-body">
+              <div className="modal-details">
+                <h1>TITLE</h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Neque, pariatur, rerum culpa fugit ipsum rem sunt dolorum
+                  animi officia iure consequatur delectus dolores explicabo nam.
+                  Exercitationem quod perferendis repellat suscipit!
+                </p>
+              </div>
+              <motion.div className="modal-buttons">
+                <div className="buttons">
+                  <MyButton
+                    name={btnCloseText}
+                    onClick={() => setSelectedId(null)}
+                  />
+                  <MyButton name={btnAccionText} />
+                </div>
+              </motion.div>
             </div>
           </motion.div>
         </motion.div>
